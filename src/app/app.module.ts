e357187d6ffcee40f4435d0ae67c31962524f592
Klaus Ferreira <claudenir.ferreira@yandex.com>
2021-03-24 16:10:52 -0300

    create addItemForm component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './index-page/index-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexPageComponent
+    IndexPageComponent,
+    AddItemFormComponent
   ],
   imports: [
     BrowserModule,
