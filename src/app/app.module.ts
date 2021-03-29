6165a3b9232df7cfedd353ba2c40dcc18c2b8014
Klaus Ferreira <claudenir.ferreira@yandex.com>
2021-03-28 21:08:21 -0300

    create and style indexPage budgetItemList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './index-page/index-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    BudgetItemListComponent
   ],
   imports: [
     BrowserModule,
