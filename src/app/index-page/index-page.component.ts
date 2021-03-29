32785316e9069bc294630938ffc309f89f1b264c
Klaus Ferreira <claudenir.ferreira@yandex.com>
2021-03-29 09:23:09 -0300

    continuing implement addItemForm inputs and button

--- src/app/index-page/index-page.component.ts
+++ src/app/index-page/index-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BudgetItem} from "../../shared/models/budget-item.model";
 
 @Component({
   selector: 'app-index-page',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index-page.component.scss']
 })
 export class IndexPageComponent implements OnInit {
+  budgetItems: BudgetItem[] = new Array<BudgetItem>()
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addItem(newItem: BudgetItem) {
+    this.budgetItems.push(newItem)
+  }
 }
